936cd19314971bdcf1481c913a47ae28e0635feb
Joshua Akande <joshuaayomikun@gmail.com>
2021-04-12 20:21:43 +0100

    made changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,10 @@ import Layout from "../layout";
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
-  return <ChakraProvider theme={theme} ><Layout><Component {...pageProps} /></Layout></ChakraProvider>
+  return <ChakraProvider theme={theme} >
+    <Layout>
+      <Component {...pageProps} /></Layout>
+    </ChakraProvider>
 }
 
 export default MyApp
